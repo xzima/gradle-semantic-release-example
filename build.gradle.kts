9333142072c09ea7363a25751c70af5c4e5c257f
Alex Zima <xzima@ro.ru>
2024-09-04 05:35:13 +0300

    chore: update copyright logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -71,7 +71,7 @@ spotless {
         licenseHeader(
             """
             /**
-             * Copyright $yearPlaceholder $author
+             * Copyright $yearPlaceholder ${author.asString()}
              *
              * Licensed under the Apache License, Version 2.0 (the "License");
              * you may not use this file except in compliance with the License.
--- src/main/kotlin/com/github/xzima/Main.kt
+++ src/main/kotlin/com/github/xzima/Main.kt
@@ -1,5 +1,5 @@
 /**
- * Copyright 2023-2024 Alex Zima
+ * Copyright 2023-2024 Alex Zima(xzima@ro.ru)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
